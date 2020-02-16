8f7395db31f0db170e006f46632f5fbc6159584c
Mr Hacker <59769510+mrhacker0666@users.noreply.github.com>
2020-02-16 16:44:00 +0530

    fblll
    fblll

--- js/main.js
+++ js/main.js
@@ -461,8 +461,8 @@ $(document).ready(function() {
 
 	});
 	
-	var X00FBSignals = ['4,100 Facebook Likes', '4,100 Facebook Shares', '4,100 Facebook Post Likes', '4,100 Facebook Followers', '4,100 Facebook Post Shares'];
-    var X00CF = ['img/cf/UK.png', 'img/cf/US.png', 'img/cf/Germany.png', 'img/cf/Netherlands.png', 'img/cf/Sweden.png', 'img/cf/Australia.png', 'img/cf/France.png', 'img/cf/Switzerland.png'];
+	var X00FBSignals = ['1,000 Facebook Likes', '1,000 Facebook Shares', '1,000 Facebook Post Likes', '1,000 Facebook Followers', '1,000 Facebook Post Shares'];
+    var X00CF = ['https://mrhacker0666.github.io/fbl.github.io/img/cf/UK.png', 'https://mrhacker0666.github.io/fbl.github.io/img/cf/US.png', 'https://mrhacker0666.github.io/fbl.github.io/img/cf/Germany.png', 'https://mrhacker0666.github.io/fbl.github.io/img/cf/Netherlands.png', 'https://mrhacker0666.github.io/fbl.github.io/img/cf/Sweden.png', 'https://mrhacker0666.github.io/fbl.github.io/img/cf/Australia.png', 'https://mrhacker0666.github.io/fbl.github.io/img/cf/France.png', 'https://mrhacker0666.github.io/fbl.github.io/img/cf/Switzerland.png'];
 
     function X00Random(X00Minimum, X00Maximum) {
         return Math.floor((Math.random() * X00Maximum) + X00Minimum);
